bb673872630866052e3d76ccd4e5084575feddba
johnsonlee <g.johnsonlee@gmail.com>
2022-06-06 14:18:18 +0800

    Fix sonatype publish issue

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,24 @@
+plugins {
+    id("io.johnsonlee.sonatype-publish-plugin") version "1.7.0"
+}
+
 tasks.register("cleanAll") {
     dependsOn(gradle.includedBuild("buildscripts").task(":cleanAll"))
     dependsOn(gradle.includedBuild("core").task(":clean"))
-    dependsOn(gradle.includedBuild("runtime").task(":clean"))
     dependsOn(gradle.includedBuild("compiler").task(":clean"))
+    dependsOn(gradle.includedBuild("runtime").task(":clean"))
     dependsOn(gradle.includedBuild("plugin").task(":clean"))
     dependsOn(":app:clean")
 }
 
-tasks.register("publishToMavenLocal") {
+tasks.register("publishAllToMavenLocal") {
     dependsOn(gradle.includedBuild("core").task(":publishToMavenLocal"))
-    dependsOn(gradle.includedBuild("runtime").task(":publishToMavenLocal"))
     dependsOn(gradle.includedBuild("compiler").task(":publishToMavenLocal"))
+    dependsOn(gradle.includedBuild("runtime").task(":publishToMavenLocal"))
 }
 
-tasks.register("publishToSonatype") {
+tasks.register("publishAllToSonatype") {
     dependsOn(gradle.includedBuild("core").task(":publishToSonatype"))
-    dependsOn(gradle.includedBuild("runtime").task(":publishReleasePublicationToSonatypeRepository"))
     dependsOn(gradle.includedBuild("compiler").task(":publishToSonatype"))
+    dependsOn(gradle.includedBuild("runtime").task(":publishReleasePublicationToSonatypeRepository"))
 }
